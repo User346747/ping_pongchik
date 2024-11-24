90a845cfe4e7e0c6387cb6d17ce70ebc3a873495
RFSkor <rfskor@gmail.com>
2024-11-24 19:06:12 +0600

    background and move characters

--- ping_pongchik.py
+++ ping_pongchik.py
@@ -8,8 +8,7 @@ window = display.set_mode((win_width, win_height))
 desk = transform.scale(image.load("desk.png"), (100, 90))
 ball = transform.scale(image.load("ball.png"), (100, 90))
 display.set_caption("Пинг понг")
-white = [200,200,255]
-window.fill(white)
+background = transform.scale(image.load("field.jpg"), (win_width, win_height))
 clock = time.Clock()
 FPS = 60
 clock.tick(FPS)
@@ -26,7 +25,7 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image,(self.rect.x, self.rect.y))
 
 
-class Player(GameSprite):
+class Player1(GameSprite):
     def __init__(self, player_image, player_x, player_y, player_speed, w, h):
         super().__init__(player_image, player_x, player_y, player_speed,w,h)
     def update(self):
@@ -34,6 +33,17 @@ class Player(GameSprite):
         if keys_pressed[K_w]:
             self.rect.y -= 5
         if keys_pressed[K_s]:
+            self.rect.y += 5
+
+
+class Player2(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, w, h):
+        super().__init__(player_image, player_x, player_y, player_speed,w,h)
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP]:
+            self.rect.y -= 5
+        if keys_pressed[K_DOWN]:
             self.rect.y += 5
 
 class Enemy(GameSprite):
@@ -48,9 +58,22 @@ class Enemy(GameSprite):
             self.rect.x = randint(5,700)
             skip += 1
 
+desk1 = Player1('desk.png', 25, win_height - 400, 40,20,180)
+
+desk2 = Player2('desk.png', 650, win_height - 200, 4,20,180)
+
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+
+    window.blit(background, (0,0))
+
+    desk1.update()
+    desk1.reset()
+    desk2.update()
+    desk2.reset()
     display.update()
+    clock.tick(FPS)
